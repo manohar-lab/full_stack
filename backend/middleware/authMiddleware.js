974d39834c2f48b570c936cfba7d49569fbcff53
manohar-lab <rmanohar.rgowda@gmail.com>
2026-04-23 11:23:48 +0530

    restructured file

--- backend/middleware/authMiddleware.js
+++
@@ -1,6 +0,0 @@
-function authMiddleware(req, res, next) {
-  // Starter middleware placeholder. Replace with JWT/session validation later.
-  next();
-}
-
-module.exports = authMiddleware;
--- frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-function Navbar() {
-  return (
-    <header className="navbar">
-      <h1>Full Stack Starter</h1>
-    </header>
-  );
-}
-
-export default Navbar;
